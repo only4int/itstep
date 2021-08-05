8975f88cf33df0d85a47c54d2bbef3f770f2933f
only4int <ip.alexnikitin@gmail.com>
2021-08-05 21:24:31 +0300

    Task 19. Examples

---
+++ tasks/Task 19. Javascript Basic/app.js
@@ -0,0 +1,71 @@
+var userHeight = 2;
+var a1 = 2, a2 = "df;klhjsklgh", a3 = 4 + 4 * 4 - 3; //однострочный комментарий
+//dfgkjdshgkljsdfgklgfjgf
+/* sdgsdagsadg */
+/*sgsdg
+sdfg
+dfg
+dfg
+dfg
+dfg
+ */
+var a = 2, b =2;
+var sum = a + b;
+var diff = a - b;
+var mul = a * b;
+var div = a / b;
+var r = a % b;
+console.log("Сумма чисел a и b равна "+sum+".");
+
+a = a + 2;
+a += 2;
+a /= a;// a = a / a;
+a++; // a = a + 1
+a--; // a = a - 1
+--a;
+++a;
+var c = 2, d = 2;
+c = c++ + 2;
+d = ++d + 2;
+console.log(c);
+console.log(d);
+
+console.log(c++ + 2);
+
+var s = "mama";
+var s2 = "papa";
+var c = s+" "+s2;
+console.log(c);
+
+var a = -2, b = 0;
+var sum = a + b; // "2"+"2"
+var diff = a - b; // 2-2
+var mul = a * b; // 2*2
+var div = a / b; // 2/2
+var r = a % b;
+var deg = a ** 0.5 + 2;// NaN + 2
+console.log(sum,diff,mul,div, deg);
+var sin = Math.acos(1);
+
+var d = true;
+var e = false;
+console.log(d, e);
+
+var k = null;
+console.log(k);
+
+var l, m = null, n = 2;
+
+console.log(l+n, m+n, l+m);
+
+var l, m = null, n = 2;
+
+console.log(l+n, m+n, l+m);
+
+console.log("  -9  " - 5);
+//apples
+x = 4;
+x = (x+1)*4/3;
+x = (x+1)*3/2;
+x = (x+1)*2/1;
+console.log(x); 
