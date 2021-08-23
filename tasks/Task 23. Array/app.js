3fee128bb9ef000c9fc2c60624dc7696d7e85a8a
only4int <ip.alexnikitin@gmail.com>
2021-08-23 21:11:12 +0300

    Task 23. Example

--- tasks/Task 23. Array/app.js
+++ tasks/Task 23. Array/app.js
@@ -24,3 +24,34 @@ for(let i = 0; i < n; i++){
     str =  str + a[i] + ", ";
 }
 console.log(str);
+
+let d = [], count = 0, j;
+for(j = 0; j < n; j++){
+    d[j] = Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(d, j, d[j-1]);
+
+for(let i = 0; i < n; i++){
+    if(d[i] % 2 == 0)
+        count++;
+}
+console.log("Количество четных элементов в массиве равно " + count);
+
+let maxElement = d[0];
+for(let i = 1; i < n; i++){
+    if(d[i] > maxElement)
+        maxElement = d[i];
+}
+console.log("Максимальный элемент равен " + maxElement);
+
+let sum = 0;
+for(let i = 0; i < n; i++){
+    sum += d[i]
+}
+console.log("Среднее арифмитическое элементов равно " + (sum/n));
+
+let reverse = [];
+for (let k = 0, i = n-1; k < n; k++, i--){
+    reverse[k] = d[i];
+}
+console.log(d,reverse);
