214db7310521fbc9eded5b19c7dffe9177afa03e
only4int <ip.alexnikitin@gmail.com>
2021-10-21 21:23:10 +0300

    Task 39. Examples

--- tasks/Task 39. DOM. Elements/js/script.js
+++ tasks/Task 39. DOM. Elements/js/script.js
@@ -2,3 +2,25 @@ let element = document.getElementById("first-element");
 console.log(element.tagName);
 console.log(element);
 element.classList.add("www");
+// - Вставьте ему в конец список ul с элементами массива [createElement, appendChild, insertBefore] так, чтобы каждый элемент стоял в своем li.
+let elements = ["createElement", "appendChild", "insertBefore"];
+
+let out = "<ul>";
+
+elements.forEach(function(item){
+    out += `<li>${item}</li>`;
+});
+
+out += "</ul>";
+
+element.innerHTML += out;
+
+let newList = document.createElement("ul");
+
+elements.forEach(function(item){
+    let newItem = document.createElement("li");
+    newItem.innerHTML = item;
+    newList.appendChild(newItem);
+});
+
+element.appendChild(newList);
