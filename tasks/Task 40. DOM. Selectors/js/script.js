4c11c134530eb7bd5e22d93900a71ac692bd0070
only4int <ip.alexnikitin@gmail.com>
2021-10-25 19:04:27 +0300

    Task 40

---
+++ tasks/Task 40. DOM. Selectors/js/script.js
@@ -0,0 +1,17 @@
+let element = document.getElementById("first-element");
+
+let elements = document.getElementsByTagName("div");
+
+console.log(elements.length);
+
+let sections = document.getElementsByClassName("first-section");
+
+let section = sections.item(0);// sections[0];
+
+let sectionDivs = section.getElementsByTagName("div");
+
+console.log(sectionDivs.length);
+
+let divs = document.querySelectorAll(".first-section div");
+
+console.log(divs.length);
