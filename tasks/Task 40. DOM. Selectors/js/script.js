de7197838380b4fbf4b4b1518b2eac974b725b7d
only4int <ip.alexnikitin@gmail.com>
2021-10-25 19:39:05 +0300

    Task 40. Example

--- tasks/Task 40. DOM. Selectors/js/script.js
+++ tasks/Task 40. DOM. Selectors/js/script.js
@@ -15,3 +15,14 @@ console.log(sectionDivs.length);
 let divs = document.querySelectorAll(".first-section div");
 
 console.log(divs.length);
+
+sections = document.getElementsByClassName("first-section");
+
+section = sections.item(0);// sections[0];
+
+let neighbors = section.getElementsByClassName("neighbor");
+
+
+for(let i = 0; i < neighbors.length; i++){
+    neighbors.item(i).innerHTML =  "Я хороший сосед";
+}
